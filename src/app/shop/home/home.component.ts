811cd16a873819ed9c2dc0c06008cfc250fbcc85
Pashmorha Anatoly <foru.paper@gmail.com>
2019-01-07 11:04:59 -0500

    shop home: show only not empty

--- src/app/shop/home/home.component.ts
+++ src/app/shop/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core'
+import { zip } from 'rxjs'
 import { Category } from 'src/app/common/common.interfaces'
 import { ProductsRequest } from 'src/app/common/productsRequest'
 import {
   CategoriesShopMockService,
 } from 'src/app/common/services/categories-shop-mock.service'
+import {
+  ProductsShopServiceMock,
+} from 'src/app/common/services/products-shop-mock.service'
 
 @Component({
   selector: 'ez-shop-home',
   template: `
     <div class="wrapper">
       <h1>Shop Categories</h1>
+      <mat-progress-bar *ngIf="loading" mode="indeterminate"></mat-progress-bar>
       <div class="flex-blocks">
         <a *ngFor="let cat of categories" [routerLink]="getLink(cat.id)">
           <mat-card matRipple>
@@ -26,11 +31,22 @@ import {
 })
 export class HomeComponent implements OnInit {
   categories: Category[]
-  constructor(private categoriesShopMockService: CategoriesShopMockService) {}
+  numberOfProducts: Array<number>
+  loading = true
+  constructor(
+    private categoriesShopMockService: CategoriesShopMockService,
+    private productsService: ProductsShopServiceMock
+  ) {}
 
   ngOnInit() {
-    this.categoriesShopMockService.getCategories().subscribe(cats => {
-      this.categories = cats
+    zip(
+      this.categoriesShopMockService.getCategories(),
+      this.productsService.countProductsForCategories()
+    ).subscribe(data => {
+      this.categories = data[0]
+      this.numberOfProducts = data[1]
+      this.categories = this.categories.filter(c => this.numberOfProducts[c.id] > 0)
+      this.loading = false
     })
   }
 
